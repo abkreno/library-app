a8e18d684a58d671b10dd4aec43e50a79b330e84
Abkreno <mohamedelhamamsy789@gmail.com>
2016-02-09 12:03:35 +0200

    Modifies contact template to change status

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,6 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    status: 'has-success',
-    glyphicon: 'glyphicon-ok'
+  emailAddress: '',
+
+  message: '',
+
+  isValidMessage: Ember.computed('message' ,function () {
+    return this.get('message').length >= 5;
+  }),
+
+  isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+
+  isDisabled: Ember.computed('isValidMessage','isValidEmail' ,function () {
+    return !(this.get('isValidEmail') && this.get('isValidMessage'));
+  }),
+
+  status: Ember.computed('isDisabled',function () {
+    return this.get('isDisabled') ?'': 'has-success';
+  }),
+
+  actions: {
+    sendMessage() {
+      this.set('responseMessage', `Thank you! your message has been sent`);
+      this.set('emailAddress', '');
+    }
+  }
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,6 +5,9 @@ export default Ember.Controller.extend({
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
+  status: Ember.computed('isValid',function () {
+    return this.get('isValid')?'has-success':'';
+  }),
 
   actions: {
 
