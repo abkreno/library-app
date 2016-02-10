6638b1d783f4dcc50a3cb584e24c96e23851b730
Abkreno <mohamedelhamamsy789@gmail.com>
2016-02-10 11:52:06 +0200

    Adds model for contact

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -21,9 +21,14 @@ export default Ember.Controller.extend({
 
   actions: {
     sendMessage() {
-      this.set('responseMessage', `Thank you! your message has been sent`);
-      this.set('emailAddress', '');
-      this.set('message', '');
+      const email = this.get('emailAddress');
+      const message = this.get('message');
+      const contact = this.store.createRecord('contact', { email: email ,message: message });
+      contact.save().then((response)=> {
+        this.set('responseMessage', `Thank you! We saved your contact with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+        this.set('message', '');
+      });
     }
   }
 });
